af4dc6b747319df889428a2258f9dcb1dfd80844
Suguna Velury <178320438+sugunav14@users.noreply.github.com>
2026-02-19 23:47:48 +0000

    input amax sync added + tested gptq super sft checkpoint
    Signed-off-by: Suguna Velury <178320438+sugunav14@users.noreply.github.com>

--- modelopt/torch/quantization/model_calib.py
+++ modelopt/torch/quantization/model_calib.py
@@ -131,6 +131,8 @@ def max_calibrate(
     for name, module in model.named_modules():
         if hasattr(module, "layer_sync_moe_local_experts_amax"):
             module.layer_sync_moe_local_experts_amax()
+        elif hasattr(module, "sync_moe_local_experts_amax"):
+            module.sync_moe_local_experts_amax()
 
     if not distributed_sync:
         return
@@ -1821,6 +1823,35 @@ def _set_input_quantizers_quant_mode(layer: nn.Module):
             module.disable_calib()
 
 
+def _set_kv_quantizers_calib_mode(layer: nn.Module):
+    for name, module in layer.named_modules():
+        if (
+            isinstance(module, TensorQuantizer)
+            and ("k_bmm_quantizer" in name or "v_bmm_quantizer" in name)
+            and not module._disabled
+            and not module._dynamic
+            and module._calibrator is not None
+        ):
+            module._calibrator.reset()
+            module.disable_quant()
+            module.enable_calib()
+
+
+def _set_kv_quantizers_quant_mode(layer: nn.Module):
+    for name, module in layer.named_modules():
+        if (
+            isinstance(module, TensorQuantizer)
+            and ("k_bmm_quantizer" in name or "v_bmm_quantizer" in name)
+            and not module._disabled
+            and not module._dynamic
+            and module._calibrator is not None
+        ):
+            if module._calibrator.compute_amax() is not None:
+                module.load_calib_amax()
+            module.enable_quant()
+            module.disable_calib()
+
+
 @contextlib.contextmanager
 def _disable_input_quantizers(layer: nn.Module):
     """Temporarily disable all enabled input quantizers in a layer."""
@@ -2017,8 +2048,17 @@ def sequential_calibrate(
 
         # Set input quantizer amaxe's for current layer modules
         _set_input_quantizers_calib_mode(layer)
+        _set_kv_quantizers_calib_mode(layer)
         inputs = gettr.get_input_activations(layer, forward_loop)
+        _set_kv_quantizers_quant_mode(layer)
         _set_input_quantizers_quant_mode(layer)
+
+        # Sync amaxes
+        for name, module in layer.named_modules():
+            if hasattr(module, "layer_sync_moe_local_experts_amax"):
+                module.layer_sync_moe_local_experts_amax()
+            elif hasattr(module, "sync_moe_local_experts_amax"):
+                module.sync_moe_local_experts_amax()
 
         # Call GPTQ
         calib_func(layer, inputs, **calib_kwargs)
