9bcd06e867cefeab289c745fda2da4a3033207af
Shengliang Xu <shengliangx@nvidia.com>
2026-03-26 00:33:12 +0000

    fix: entry is a dict
    Signed-off-by: Shengliang Xu <shengliangx@nvidia.com>

--- examples/llm_ptq/hf_ptq.py
+++ examples/llm_ptq/hf_ptq.py
@@ -329,7 +329,7 @@ def auto_quantize(
         ),
         verbose=True,
         # Disable all default disabled layers such as lm_head, mlp.gate, router etc.
-        disabled_layers=[entry.quantizer_path for entry in _default_disabled_quantizer_cfg],
+        disabled_layers=[entry["quantizer_path"] for entry in _default_disabled_quantizer_cfg],
         method=auto_quantize_method,
         checkpoint=auto_quantize_checkpoint,
     )
