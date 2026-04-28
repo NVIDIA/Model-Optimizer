816ddfaa063ffd355ae8369eebdf469ae098c8dc
Grzegorz Karch <gkarch@nvidia.com>
2026-04-28 00:29:17 -0700

    enabling runtime optimization
    Signed-off-by: Grzegorz Karch <gkarch@nvidia.com>

--- modelopt/torch/nas/subblock_stats/__init__.py
+++ modelopt/torch/nas/subblock_stats/__init__.py
@@ -12,7 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Subblock statistics collection for Puzzletron."""
-
-from .calc_subblock_stats import *
+from .calc_runtime_stats import calc_runtime_for_subblocks
