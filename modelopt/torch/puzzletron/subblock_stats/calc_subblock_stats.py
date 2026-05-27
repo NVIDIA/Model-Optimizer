333214995c9ed9d9e6711eb6a326ceb077abcc1e
Grzegorz Karch <gkarch@nvidia.com>
2026-05-27 08:33:05 -0700

    reenabled some vars
    Signed-off-by: Grzegorz Karch <gkarch@nvidia.com>

--- modelopt/torch/puzzletron/subblock_stats/calc_subblock_stats.py
+++ modelopt/torch/puzzletron/subblock_stats/calc_subblock_stats.py
@@ -33,12 +33,13 @@ from omegaconf import DictConfig, ListConfig, OmegaConf
 from tqdm import tqdm
 from transformers import PretrainedConfig
 
+from modelopt.torch.utils import json_dump
+
 from ..anymodel.model_descriptor import ModelDescriptor, ModelDescriptorFactory
 from ..block_config import AttentionConfig, BlockConfig, FFNConfig, SubblockConfig
 from ..replacement_library.replacement_utils import parse_layer_replacement
 from ..tools.checkpoint_utils import load_model_config
 from ..tools.logger import mprint
-from ..utils import json_dump
 from ..utils.parsing import format_global_config
 from .calc_subblock_params_and_memory import (
     calc_subblock_active_params,
