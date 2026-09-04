b8eeb30255671c038f78889508bd11c3f2f6b01d
Meng Xin <mxin@nvidia.com>
2026-09-03 23:44:54 -0700

    chore: document Qwen Image source attribution
    Signed-off-by: Meng Xin <mxin@nvidia.com>

--- modelopt/torch/fastgen/plugins/qwen_image_pdd.py
+++ modelopt/torch/fastgen/plugins/qwen_image_pdd.py
@@ -1,7 +1,19 @@
-# Adapted from the Qwen-Image implementation in Diffusers:
-# https://github.com/huggingface/diffusers/blob/275869dcae4ebcfee6a80253fdabc56033335020/src/diffusers/models/transformers/transformer_qwenimage.py
-# The masked joint-attention execution follows FastGen merge request 210.
-# SPDX-FileCopyrightText: Copyright (c) 2025 Qwen-Image Team, The HuggingFace Team. All rights reserved.
+# Adapted from https://github.com/huggingface/diffusers/blob/275869dcae4ebcfee6a80253fdabc56033335020/src/diffusers/models/transformers/transformer_qwenimage.py
+
+# Copyright 2025 Qwen-Image Team, The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -17,7 +29,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Qwen-Image adapter and explicit output-projection conversion for PDD."""
+"""Qwen-Image adapter and explicit output-projection conversion for PDD.
+
+The masked joint-attention execution follows NVIDIA FastGen merge request 210.
+"""
 
 from __future__ import annotations
 
